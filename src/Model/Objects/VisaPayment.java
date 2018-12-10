ab4af51ebe5ec29468cb379fba2192858340e342
TheMightyJew <38401349+TheMightyJew@users.noreply.github.com>
2018-12-11 00:56:22 +0200

    added payment fxml and controller

--- src/Model/Objects/VisaPayment.java
+++ src/Model/Objects/VisaPayment.java
@@ -1,15 +1,23 @@
 package Model.Objects;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 public class VisaPayment extends Payment {
     private int cardNumber;
     private int threeNumbers;
-    private Date date;
+    private LocalDate date;
     private String ownerId;
     private String ownerFirstName;
     private String ownerLastName;
 
+    public VisaPayment(int cardNumber, int threeNumbers, LocalDate date, String ownerId, String ownerFirstName, String ownerLastName) {
+        this.cardNumber = cardNumber;
+        this.threeNumbers = threeNumbers;
+        this.date = date;
+        this.ownerId = ownerId;
+        this.ownerFirstName = ownerFirstName;
+        this.ownerLastName = ownerLastName;
+    }
 
     @Override
     public boolean isPaypal() {
--- src/View/Main.java
+++ src/View/Main.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 
@@ -14,6 +15,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         FXMLLoader fxmlLoader = new FXMLLoader();
+        //primaryStage.getIcons().add(new Image(this.getClass().getResourceAsStream("vacationPic2.jpg")));
         Parent root = fxmlLoader.load(getClass().getResource("View.fxml").openStream());
         ViewController viewController =fxmlLoader.getController();
         Model model=new Model("Vaction4U");
